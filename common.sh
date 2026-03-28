91eb628c56d672147f064d6618f20caab3f150dd
unknown <surendravarmajampana.59@gmail.com>
2026-03-28 20:38:07 +0530

    shell-script

--- common.sh
+++ common.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
@@ -9,6 +8,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
 
 VALIDATE(){
     if [ $1 -ne 0 ]
